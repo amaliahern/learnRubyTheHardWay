be6abd8959f598f471f65a8cc8abe9e6540fb680
Amalia Hernandez Garcia <amalia@Amalias-MacBook-Pro.local>
2015-04-20 22:41:10 +0200

    First commit to add exi1.rb

---
+++ exi1.rb
@@ -0,0 +1,8 @@
+puts "HEllo World!"
+#puts "Hello again!"
+#puts "I lkie typing this"
+#puts "this is fun"
+#puts "YAy! Printing"
+#puts "I'd much rather you 'not'"
+puts 'I "said" you don´t touch this'
+#puts "print another line"
