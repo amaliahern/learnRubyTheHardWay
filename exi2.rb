c899e94253169e5ae68a1eccd063af6f3b713700
Amalia Hernandez Garcia <amalia@Amalias-MacBook-Pro.local>
2015-04-20 23:10:48 +0200

    Adding second exercise, exi2.rb

---
+++ exi2.rb
@@ -0,0 +1,8 @@
+#A comment this is so you can read your program later
+#Anything after #, will be ignored by ruby
+puts "I have code like this" #and the comment after is ignored
+
+#You can also used comments to "disable" or comment out piece of code
+#puts "This won´t run"
+
+puts "This will run"
